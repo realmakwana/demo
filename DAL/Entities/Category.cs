37e8effd28fdfce15f63b7be1b15c7d09ea5fdac
Gopal Makwana <realmakwana@gmail.com>
2026-02-03 11:21:15 +0530

    03-02-2026-aedit

--- DAL/Entities/Category.cs
+++ DAL/Entities/Category.cs
@@ -8,6 +8,8 @@ public class Category
 {
     [Key]
     public Int32 CatID { get; set; }
+    
+    [Required(ErrorMessage = "Category Name is required")]
     public String CatName { get; set; }
     public int CatTypeID { get; set; }
     public bool IsActive { get; set; }
